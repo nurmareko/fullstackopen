0bbb81253ba6d7cf7ef9b84d3c1bc2db1df5e3e6
nurmareko <nurmareko@yandex.ru>
2025-01-28 23:52:54 +0700

    Part 1c workthrough
    Page re-rendering

---
+++ part1/part1c/src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+  root.render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter++
+}, 1000)

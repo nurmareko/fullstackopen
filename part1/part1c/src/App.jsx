3880d070bad34f2d591fd5d8b3dc9021c6063411
nurmareko <nurmareko@yandex.ru>
2025-01-29 21:19:35 +0700

    Part 1c workthrough
    Refactoring the components

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -1,7 +1,43 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
   )
 }
 

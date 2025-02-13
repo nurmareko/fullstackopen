5a7d65bb064319b942ec460ad97691e3f34d6e9f
nurmareko <nurmareko@yandex.ru>
2025-02-13 22:04:04 +0700

    Implement Content component to render contents
    Complete exercise 2.1 Course information step 6

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,9 +1,24 @@
 const Header = (props) => <h1>{props.course}</h1>
 
+const Content = ({ parts }) => {
+  const render = (part) => {
+    return (
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>
+    )
+  }
+  
+  return (
+    parts.map(render)
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
     <Header course={course.name} />
+    <Content parts={course.parts} />
     </div>
   )
 }

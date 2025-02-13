3d7b78739393a0419e16721895148f88d32aba53
nurmareko <nurmareko@yandex.ru>
2025-02-13 21:40:36 +0700

    Reinitialize App and Main with correct the code

--- part2/courseinfo/src/main.jsx
+++ part2/courseinfo/src/main.jsx
@@ -1,27 +1,5 @@
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+import ReactDOM from 'react-dom/client'
 
-  return <Course course={course} />
-}
+import App from './App'
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)

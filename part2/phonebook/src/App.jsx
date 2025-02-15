cfe9db7af35dd75768b01728156098b6fc5b2736
nurmareko <nurmareko@yandex.ru>
2025-02-15 20:42:23 +0700

    Add ability to render persons state

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      id: 1,
+      name: 'Arto Hellas'
+    },
+    {
+      id: 2,
+      name: 'Astrid Noah'
+    }
   ])
   const [newName, setNewName] = useState('')
 
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
       <form>
         <div>
@@ -18,7 +26,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <p key={person.id}>{person.name}</p>
+      )}
     </div>
   )
 }

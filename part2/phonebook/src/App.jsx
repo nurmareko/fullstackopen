22ff430a7b891f09273e1b302df02562a36c0dca
nurmareko <nurmareko@yandex.ru>
2025-02-16 20:16:21 +0700

    Use person.name as key

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      id: 1,
-      name: 'Arto Hellas'
+      name: "Arto Hellas",
     },
     {
-      id: 2,
-      name: 'Astrid Noah'
-    }
-  ])
-  const [newName, setNewName] = useState('')
+      name: "Astrid Noah",
+    },
+  ]);
+  const [newName, setNewName] = useState("");
 
   return (
     <div>
@@ -26,11 +24,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
-        <p key={person.id}>{person.name}</p>
-      )}
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

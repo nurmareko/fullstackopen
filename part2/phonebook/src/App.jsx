83b0fc2de41f7d9b466d0a03d897f7b71bd7aa99
nurmareko <nurmareko@yandex.ru>
2025-02-16 20:58:24 +0700

    Implement saving new person functionality
    Compolete exercise 2.6: phonebook Step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -8,16 +8,29 @@ const App = () => {
     {
       name: "Astrid Noah",
     },
-  ]);
-  const [newName, setNewName] = useState("");
+  ])
+  const [newName, setNewName] = useState("")
+
+  const addPerson = () => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleNameChange = () => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -28,7 +41,7 @@ const App = () => {
         <p key={person.name}>{person.name}</p>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

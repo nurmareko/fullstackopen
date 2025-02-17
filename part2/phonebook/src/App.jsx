fb5f95d0a70d06d67b1ebb53d7703c25c55fcb14
nurmareko <nurmareko@yandex.ru>
2025-02-17 14:55:23 +0700

    Expand App to allow users to add phone numbers to the phone book.
    Complete exercise 2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react"
+import RenderPersons from "./components/RenderPersons"
 
 const App = () => {
   const [persons, setPersons] = useState([
     {
       name: "Arto Hellas",
+      number: "040-1234567"
     },
     {
       name: "Astrid Noah",
+      number: "1337-666"
     },
   ])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const addPerson = () => {
     event.preventDefault()
     const trimedName = newName.trim()
 
     if (isNameExist(trimedName)) {
-      alert(trimedName + ' is already added to phonebook')
+      alert(trimedName + " is already added to phonebook")
     }
     else {
       const newPerson = {
-        name: trimedName
+        name: trimedName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
     }
@@ -31,28 +36,31 @@ const App = () => {
   }
 
   const isNameExist = (name) => {
-    // IMPROVE: handdle trailing whitespace
     return (
       persons.some(person => person.name === name)
     )
   }
 
+  const handleNumberChange = () => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
-      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
+      <RenderPersons persons={persons} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/RenderPersons.jsx
@@ -0,0 +1,7 @@
+const RenderPersons = ({ persons }) => {
+  return (
+    persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+  )
+}
+
+export default RenderPersons

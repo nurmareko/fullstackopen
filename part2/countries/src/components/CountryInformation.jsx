9fa3ff623b8ea5631266badbc18689f4bae00e86
nurmareko <nurmareko@yandex.ru>
2025-10-04 23:44:01 +0700

    Add country information display component

--- part2/countries/src/components/CountryInformation.jsx
+++ part2/countries/src/components/CountryInformation.jsx
@@ -1,5 +1,24 @@
 const CountryInformation = ({ country }) => {
-  return <p>country information</p>
+  const name = country.name.common
+  const capital = country.capital[0]
+  const areaCode = country.area
+  const languages = country.languages
+  const flagUrl = country.flags.png
+
+  return (
+    <>
+      <h1>{name}</h1>
+      <p>Capital {capital}</p>
+      <p>Area {areaCode}</p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(languages).map((name, index) =>
+          <li key={index}>{name}</li>
+        )}
+      </ul>
+      <img alt="country flag" src={flagUrl} />
+    </>
+  )
 }
 
 export default CountryInformation
--- part2/countries/src/components/Information.jsx
+++ part2/countries/src/components/Information.jsx
@@ -1,18 +1,27 @@
 import CountryInformation from './CountryInformation'
 import ListOfCountry from './ListOfCountry'
 
-const Information = ({ countries }) => {
-  const length = countries.length
-
-  if (length == 0) {
+const Information = ({ countries, countryFilter }) => {
+  if (countryFilter === "") {
     return <></>
-  } else if (length == 1) {
-    return <CountryInformation country={countries[0]} />
-  } else if (length <= 10) {
-    return <ListOfCountry countries={countries} />
   } else {
-    return <p>Too many matches, specify another filter</p>
+    const coutriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(countryFilter.toLowerCase()))
+    const length = coutriesToShow.length
+    console.log(length)
+
+    if (length == 0) {
+      return <></>
+    } else if (length == 1) {
+      console.log(coutriesToShow[0])
+      return <CountryInformation country={coutriesToShow[0]} />
+    } else if (length <= 10) {
+      return <ListOfCountry countries={countries} />
+    } else {
+      return <p>Too many matches, specify another filter</p>
+    }
   }
+
+
 }
 
 export default Information

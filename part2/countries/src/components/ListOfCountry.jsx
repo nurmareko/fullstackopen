42403e40b9eba43d7b8ae364c7769842d973d709
nurmareko <nurmareko@yandex.ru>
2025-10-06 10:31:00 +0700

    Add show button to country list items

--- part2/countries/src/components/ListOfCountry.jsx
+++ part2/countries/src/components/ListOfCountry.jsx
@@ -2,7 +2,7 @@ const ListOfCountry = ({ countries }) => {
   const names = countries.map(country => country.name.common)
   return (
     <>
-      {names.map(name => <p>{name}</p>)}
+      {names.map(name => <p>{name} <button>Show</button></p>)}
     </>
   )
 }

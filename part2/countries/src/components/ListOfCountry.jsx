5c0977d442e56f6a192f7e36ca75e05050087a88
nurmareko <nurmareko@yandex.ru>
2025-10-06 10:52:38 +0700

    Add key prop to country list items

--- part2/countries/src/components/ListOfCountry.jsx
+++ part2/countries/src/components/ListOfCountry.jsx
@@ -2,7 +2,7 @@ const ListOfCountry = ({ countries, showPerson }) => {
   const names = countries.map(country => country.name.common)
   return (
     <>
-      {names.map(name => <p>{name} <button onClick={() => showPerson(name)}>Show</button></p>)}
+      {names.map(name => <p key={name}>{name} <button onClick={() => showPerson(name)}>Show</button></p>)}
     </>
   )
 }

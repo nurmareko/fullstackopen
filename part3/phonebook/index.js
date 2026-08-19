ef3483f19aca8da7783b0af52d9373f007f9d1e7
nurmareko <nurmareko@yandex.ru>
2026-08-20 00:03:16 +0700

    Log request bodies with Morgan middleware
    Complete exercise 3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
-app.use(morgan('tiny'))
-
 let phonebook = [
     {
       "id": "1",
@@ -27,6 +24,10 @@ let phonebook = [
       "number": "39-23-6423122"
     }
 ]
+
+app.use(express.json())
+morgan.token('request-body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :request-body`))
 
 app.get('/info', (request, response) => {
   const n = phonebook.length
@@ -98,7 +99,6 @@ app.post('/api/persons', (request, response) => {
   }
   phonebook = phonebook.concat(person)
   response.json(person)
-  console.log(person)
 })
 
 const PORT = 3001

24327e39462f8a0102f824561d48cd7ab0aa0bb2
nurmareko <nurmareko@yandex.ru>
2026-08-19 22:13:40 +0700

    Add POST endpoint for creating phonebook entries
    Complete exercise 3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let phonebook = [
     {
       "id": "1",
@@ -53,6 +55,36 @@ app.delete('/api/persons/:id', (request, response) => {
   phonebook = phonebook.filter(person => person.id != id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const MAX = 1000000
+
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * MAX)
+  }
+
+  return String(getRandomInt(MAX))
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  // Validate data
+  if (!(body.name && body.number)) {
+    return response.status(400).json(
+      {error: 'invalid data'}
+    )
+  }
+
+  const person = {
+    "id": generateId(),
+    "name": body.name,
+    "number": body.number
+  }
+  phonebook = phonebook.concat(person)
+  response.json(person)
+  console.log(person)
 })
 
 const PORT = 3001

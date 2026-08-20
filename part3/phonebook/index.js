e44f19eb1be0b6beca83f5314d586c0eb5e9e9df
nurmareko <nurmareko@yandex.ru>
2026-08-21 02:05:38 +0700

    Use Environment Port for Phonebook Server

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 let phonebook = [
@@ -106,7 +107,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

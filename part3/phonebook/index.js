98e17772f4c6b8ba092317fca63e84bd4dc4b18f
nurmareko <nurmareko@yandex.ru>
2026-08-21 01:41:31 +0700

    Serve the phonebook frontend build
    Complete exercise 3.11 Full Stack Phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,6 +25,7 @@ let phonebook = [
     }
 ]
 
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('request-body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :request-body`))

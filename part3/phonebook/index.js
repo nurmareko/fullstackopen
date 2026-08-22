af88f72fd4bb4b9ed6086b14d4ded0ceb085fd93
nurmareko <nurmareko@yandex.ru>
2026-08-22 15:13:11 +0700

    Use MongoDB to Fetch Persons by ID

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -61,16 +61,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  person = phonebook.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(existedPerson => {
+      if (existedPerson) {
+        return response.json(existedPerson)
+      } else {
+        return response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
